f5765aa2ff97eda9ee8ab12114e5898224ecae0a
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-02-26 23:23:03 +0530

    create input variable to take input

--- src/HuffmanCompression.java
+++ src/HuffmanCompression.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class HuffmanCompression {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any text : ");
+        String input = scanner.nextLine();
 
     }
 }
